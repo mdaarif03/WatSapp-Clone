146d9ed7c77d1efc980b8ca04b06f0f29e8835a8
mdaarif03 <mdaarifraza03@gmail.com>
2023-11-10 20:42:58 +0530

    hii

--- server/index.js
+++ server/index.js
@@ -4,8 +4,8 @@ import bodyParser from "body-parser";
 
 import Connection from "./database/db.js";
 import Route from "./routes/route.js";
+import path from 'path';
 
-// const path = require("path");
 const app = express();
 
 app.use(cors());
@@ -15,21 +15,25 @@ app.use("/", Route);
 
 Connection();
 
-// -----------------------------Deployment----------------
-
-// const __dirname1 = path.resolve()
-// if (process.env.NODE_ENV==="production") {
-//   app.use(express.static(path.join(__dirname1,"/client/build")));
-
-//   app.get('*',(req,res) =>{
-//     res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
-//   });
-
-// }else{
-//   app.get("/", (req, res) => {
+// app.get("/", (req, res) => {
 //     res.send("API is running...");
 //   });
-// }
+
+// -----------------------------Deployment----------------
+
+const __dirname1 = path.resolve()
+if (process.env.NODE_ENV==="production") {
+  app.use(express.static(path.join(__dirname1,"/client/build")));
+
+  app.get('*',(req,res) =>{
+    res.sendFile(path.resolve(__dirname1,"client","build","index.html"));
+  });
+
+}else{
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 // -----------------------------Deployment----------------
 
